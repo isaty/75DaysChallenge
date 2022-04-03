a7ace98a1c1acc4db994264ce4740773d5135be2
Satyam Singh <41682264+isaty@users.noreply.github.com>
2022-04-03 17:03:15 +0530

    Time: 570 ms (5.04%), Space: 103.7 MB (93.14%) - LeetHub

---
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -0,0 +1,76 @@
+class RandomizedCollection {
+public:
+    unordered_map<int,vector<int>>mp;
+    vector<int>arr;
+    
+    RandomizedCollection() { 
+    }
+    
+    bool insert(int val) {
+        bool k=true;
+        
+        if(mp.find(val)!=mp.end() && mp[val].size()>0)
+        {
+            cout<<mp[val].size();
+            k=false;}
+        
+        mp[val].push_back(arr.size());
+        arr.push_back(val);
+        
+        return k;
+    }
+    
+    bool remove(int val) {
+        
+        if(mp.find(val)==mp.end() || mp[val].size()<=0)
+            return false;
+        
+        int mp_idx=mp[val].size()-1;
+        int vec_idx=mp[val][mp_idx];
+        // cout<<vec_idx;
+        //last element
+        int last=arr.back();
+        int mp_lidx=mp[last].size()-1;
+        
+        if(last==val)
+        {
+            
+            mp[last].pop_back();
+            arr.pop_back();
+            // cout<<mp[val].size()<<" "<<mp[last].size();
+            return true;
+        }
+        
+        //swapping
+        swap(arr[vec_idx],arr[arr.size()-1]);
+        
+        //delete from vector
+        arr.pop_back();
+        
+        //delete val fromt map
+        mp[val].pop_back();
+        
+        //delete last from map
+        mp[last].pop_back();
+        
+        //insert val's index in map
+        mp[last].push_back(vec_idx);
+        
+        //sort mp[last] 
+        sort(mp[last].begin(),mp[last].end());
+        
+        return true;
+    }
+    
+    int getRandom() {
+        return arr[rand()%arr.size()];
+    }
+};
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection* obj = new RandomizedCollection();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
