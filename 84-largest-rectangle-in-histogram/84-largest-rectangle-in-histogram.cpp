6d61a6bcaeb5c4af1165a7c16b768de7d95e371a
Satyam Singh <41682264+isaty@users.noreply.github.com>
2022-04-18 10:02:20 +0530

    Time: 198 ms (73.91%), Space: 81.5 MB (37.85%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -5,48 +5,41 @@ public:
         vector<int>left(n,0);
         vector<int>right(n,0);
         
+        int mx=0;
         stack<int>st;
         for(int i=0;i<n;i++)
         {
-            while(!st.empty() && heights[st.top()]>=heights[i])
-            {
+            while(!st.empty() && heights[i]<=heights[st.top()])
                 st.pop();
-            }
-            
+    
             if(st.empty())
                 left[i]=-1;
             else
                 left[i]=st.top();
-            
             st.push(i);
         }
         
-        stack<int>st2;
+    
+        stack<int>st1;    
         for(int i=n-1;i>=0;i--)
         {
-            while(!st2.empty() && heights[st2.top()]>=heights[i])
-            {
-                st2.pop();
-            }
-            if(st2.empty())
+            while(!st1.empty() && heights[i]<=heights[st1.top()])
+                st1.pop();
+            
+            if(st1.empty())
                 right[i]=n;
             else
-                right[i]=st2.top();
+                right[i]=st1.top();
             
-            st2.push(i);
+            st1.push(i);
         }
-        // cout<<right[n-1];
-        int ans=0;
+        
         for(int i=0;i<n;i++)
         {
-            // cout<<left[i]<<" "<<right[i]<<"\n";
-            int l=heights[left[i]+1];
-            int r=heights[right[i]-1];
-            
-            int temp=min(heights[i],min(l,r))*(right[i]-left[i]-1);
-            ans=max(temp,ans); 
-            
+            int area=heights[i]*(right[i]-left[i]-1);
+            mx=max(mx,area);
         }
-        return ans;
+         
+        return mx;
     }
 };
