c50673678b2d2d5e3e4092c4fcc70b0e9feea606
Satyam Singh <41682264+isaty@users.noreply.github.com>
2022-04-18 16:53:34 +0530

    Time: 476 ms (33.28%), Space: 189.4 MB (14.40%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-    
+        list<int>dq;
         vector<int>res;
-        map<int,int,greater<int>>mp;
         
-        for(int i=0;i<k;i++)
+        int i=0,j=0;
+        while(j<nums.size())
         {
-            mp[nums[i]]++;
+            if(j-i==k)
+            { 
+               res.push_back(nums[dq.front()]);
+               if(dq.front()==j-k)
+               {
+                   dq.pop_front();
+               }
+               i++;
+            }
+            
+            while(!dq.empty() && nums[dq.back()]<nums[j])
+                dq.pop_back();
+            
+            dq.push_back(j);
+            j++;
         }
         
-        auto j=mp.begin();
-        pair<int,int>p=*j;
-        res.push_back(p.first);
-        
-        for(int i=k;i<nums.size();i++)
-        {
-            mp[nums[i-k]]--;
-            
-            if(mp[nums[i-k]]==0)
-            {
-                // cout<<mp.size();
-                mp.erase(nums[i-k]);}
-            
-            
-            
-            mp[nums[i]]++;
-            auto k=mp.begin();
-            pair<int,int>l=*k;
-            res.push_back(l.first);
-        }
-        
+        res.push_back(nums[dq.front()]);
         return res;
     }
 };
