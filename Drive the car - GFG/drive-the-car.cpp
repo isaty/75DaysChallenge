51d655521092055e219e204311977a2ce35d8352
Satyam Singh <41682264+isaty@users.noreply.github.com>
2022-05-01 16:35:00 +0530

    Added solution - LeetHub

---
+++ Drive the car - GFG/drive-the-car.cpp
@@ -0,0 +1,32 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+        long long required(long long arr[], long long n, long long k) {
+        // Your code goes here
+        
+        long long mx=*max_element(arr,arr+n);
+        
+        return (mx<=k)? -1:(mx-k);
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long n, k;
+        cin >> n >> k;
+        long long arr[n];
+        for (long long i = 0; i < n; i++) cin >> arr[i];
+        Solution ob;
+        cout << ob.required(arr, n, k) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
