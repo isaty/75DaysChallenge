5775e594b5570aa3e77b363b904904bbdb976615
Satyam Singh <41682264+isaty@users.noreply.github.com>
2022-05-24 19:55:26 +0530

    Time: 287 ms (35.02%), Space: 54.7 MB (61.98%) - LeetHub

---
+++ 310-minimum-height-trees/310-minimum-height-trees.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        vector<int>in(n,0);
+        vector<int>adj[n];
+        vector<int>res;
+        queue<int>q;
+        
+        
+        for(vector<int>i:edges)
+        {
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+            in[i[1]]++;
+            in[i[0]]++;
+        }//egdes to graph
+        
+       
+        
+        for(int i=0;i<n;i++)
+        {
+            if(in[i]<=1)
+            {
+                q.push(i);
+            }
+        }//initial q push
+        
+        while(!q.empty())
+        {
+            res.clear();
+            int k=q.size();
+            for(int i=0;i<k;i++)
+            {
+                int t=q.front();
+                q.pop();
+                res.push_back(t);
+                for(int u:adj[t])
+                {
+                    in[u]--;
+                    if(in[u]==1)
+                        q.push(u);
+                }
+            }
+        }
+        
+        return res;
+        
+    }
+};
