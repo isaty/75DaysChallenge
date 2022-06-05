0a9714d9784435eaa3572ef84295e1d4a96ae177
Satyam Singh <41682264+isaty@users.noreply.github.com>
2022-06-05 17:57:30 +0530

    Time: 11 ms (19.66%), Space: 9.2 MB (45.56%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -5,22 +5,15 @@ public:
         int j=0;
         for(char c:strs[0])
         {
-            bool l=true;
             for(int k=1;k<strs.size();k++)
             {
                if(c!=strs[k][j])
                {
-                   l=false;
-                   break;
+                   return ans;
                }
-            }
-            
-            if(l)
-                ans+=c;
-            else 
-                break;
-            
-            j++;
+            } 
+           ans+=c;
+           j++;
         }
         return ans;
     }
